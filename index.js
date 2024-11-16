bf7d64191ee086d4be96baac9b8c825aed67739f
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2024-11-16 20:54:25 +0500

    solved exercise 3.5 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require("morgan")
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(express.json());
 
 let persons = [
@@ -51,7 +53,34 @@ app.delete('/api/persons/:id', (request, response) => {
 
   response.status(204).end()
 })
-
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  const id = Math.random(0, 1010100101)
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number is missing' 
+    })
+  }
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+  persons = persons.concat(person);
+  response.json(persons);
+});
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
